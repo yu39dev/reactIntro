bc02d675904d7efbcae85f66f43ac73fb681e3bf
yu39dev <102802128+yu39dev@users.noreply.github.com>
2022-05-13 09:32:06 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello, React</h1>
+      <p>How are you?</p>
+    </>
+  );
+};
+
+export default App; //他のファイルで使うために（＝コンポーネント化のために）明示
